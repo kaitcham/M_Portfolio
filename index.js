f719f1e19fe84d240d9b688d073d367cbda859e2
Munezero Christian <chamsf5@aol.com>
2022-03-01 15:36:13 +0200

    Add closeMenu function and close menu when link clicked

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let navMenu = document.querySelector(".navbar"); 
 let navIcon = document.querySelector(".fa-bars"); 
+let navLink = document.querySelectorAll(".nav-items li a");
 
 function displayMenu() { 
   navMenu.classList.toggle("menu-active"); 
   navIcon.classList.add("is-active");
 } 
+
+function closeMenu() {
+  navMenu.classList.toggle("menu-active");
+  navIcon.classList.remove("is-active");
+}
+
+[].forEach.call(navLink, function (link) {
+  link.onclick = () => {
+    navMenu.classList.toggle("menu-active");
+    navIcon.classList.toggle("is-active");
+  };
+});
