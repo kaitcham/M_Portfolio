332672c5251b0f595e8497a37118e649a740037f
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-03 11:42:27 +0100

    Add a popup feature

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-import projectsArray from "./data.js";
+import projectsArray from './data.js'
 
-const body = document.querySelector("body");
-const navMenu = document.querySelector(".navbar");
-const navIcon = document.querySelector(".fa-bars");
-const navLink = Array.from(document.querySelectorAll(".nav-items li a"));
+const body = document.querySelector('body')
+const navMenu = document.querySelector('.navbar')
+const navIcon = document.querySelector('.fa-bars')
+const navLink = Array.from(document.querySelectorAll('.nav-items li a'))
 
 window.displayMenu = () => {
-  navMenu.classList.toggle("menu-active");
-  navIcon.classList.add("is-active");
-  body.style.overflow = "hidden";
-};
+  navMenu.classList.toggle('menu-active')
+  navIcon.classList.add('is-active')
+  body.style.overflow = 'hidden'
+}
 
 window.closeMenu = () => {
-  navMenu.classList.toggle("menu-active");
-  navIcon.classList.remove("is-active");
-  body.style.overflow = "visible";
-};
+  navMenu.classList.toggle('menu-active')
+  navIcon.classList.remove('is-active')
+  body.style.overflow = 'visible'
+}
 
 navLink.forEach.call(navLink, (link) => {
   link.onclick = () => {
-    navMenu.classList.toggle("menu-active");
-    navIcon.classList.toggle("is-active");
-    body.style.overflow = "visible";
-  };
-});
+    navMenu.classList.toggle('menu-active')
+    navIcon.classList.toggle('is-active')
+    body.style.overflow = 'visible'
+  }
+})
 
-const projectCards = document.querySelector(".project");
-let htmlCode = "";
+const projectCards = document.querySelector('.project')
+let htmlCode = ''
 
 projectsArray.forEach((project) => {
-  const { title, image, imageAlt, info, shortDesc, languages, more } = project;
+  const { title, image, imageAlt, info, shortDesc, languages, more } = project
 
   if (projectsArray.indexOf(project) % 2 === 0) {
     htmlCode += `
@@ -44,9 +44,9 @@ projectsArray.forEach((project) => {
             .map((item) =>
               item === info[0]
                 ? `<li>${item}</li>`
-                : `<li class='project-circle'></li><li>${item}</li>`
+                : `<li class='project-circle'></li><li>${item}</li>`,
             )
-            .join("")}
+            .join('')}
         </ul>
         <p class='project-desc'>
           ${shortDesc}
@@ -54,14 +54,14 @@ projectsArray.forEach((project) => {
         <ul class='project-tech'>
           ${languages
             .map((item) => `<li class='project-tag'>${item}</li>`)
-            .join("")}
+            .join('')}
         </ul>
         <button id='${title}' type='button' class='project-button' onclick='showPopup("${title}")'>
           ${more}
         </button>
       </div>
     </div>
-  `;
+  `
   } else {
     htmlCode += `
     <div class='card'>
@@ -72,9 +72,9 @@ projectsArray.forEach((project) => {
             .map((item) =>
               item === info[0]
                 ? `<li>${item}</li>`
-                : `<li class='project-circle'></li><li>${item}</li>`
+                : `<li class='project-circle'></li><li>${item}</li>`,
             )
-            .join("")}
+            .join('')}
         </ul>
         <p class='project-desc'>
           ${shortDesc}
@@ -82,7 +82,7 @@ projectsArray.forEach((project) => {
         <ul class='project-tech'>
           ${languages
             .map((item) => `<li class='project-tag'>${item}</li>`)
-            .join("")}
+            .join('')}
         </ul>
         <button id='${title}' class='project-button' type='button' onclick='showPopup("${title}")'>
           ${more}
@@ -92,8 +92,35 @@ projectsArray.forEach((project) => {
         <img src='${image}' alt='${imageAlt}' />
       </div>
     </div>
-  `;
+  `
   }
-});
+})
 
-projectCards.innerHTML = htmlCode;
+projectCards.innerHTML = htmlCode
+
+const popup = document.querySelector('.showPopup')
+let popupCode = ''
+
+window.showPopup = (string) => {
+  const project = projectsArray.filter((project) => project.title === string)
+  popup.style.display = 'block'
+  body.style.overflow = 'hidden'
+  project.forEach((elem) => {
+    const { title, image, imageAlt, info, longDesc, languages } = elem
+    popupCode += ` <div class='popup-container'> <div class='popup'> <p><i class='fa-solid fa-xmark close'></i></p> <div class='popup-card'> <div class='popup-title'> <h2>${title}</h2> </div> <ul class='project-details'> ${info
+      .map((item) =>
+        item === info[0]
+          ? `<li>${item}</li>`
+          : `<li class='project-circle'></li><li>${item}</li>`,
+      )
+      .join(
+        '',
+      )} </ul> <div class='popup-image'> <img src='${image}' alt='${imageAlt}' /> </div> <div class='popup-description'> <p> ${longDesc} </p><div class='popup-languages'> <ul class='project-tech'> ${languages
+      .map((item) => `<li class='project-tag'>${item}</li>`)
+      .join(
+        '',
+      )} </ul> <hr class='popup-skills' /> <div class='popup-buttons'> <button class='popup-btn' type='button'>See Live</button> <button class='popup-btn' type='button'>See Source</button> </div> </div> </div> </div> </div> </div> `
+  })
+  popup.innerHTML = popupCode
+}
+
